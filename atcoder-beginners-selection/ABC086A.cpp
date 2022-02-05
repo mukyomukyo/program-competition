70fdf67501221428255b6e16dc38a90efbfa0ce0
mukyomukyo <hanjirohakone@gmail.com>
2022-02-05 11:40:54 +0900

    helloworld

---
+++ atcoder-beginners-selection/ABC086A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    if (a % 2 == 1){
+        if (b % 2 == 1){
+            cout << "Odd" << endl; 
+        }
+        else{
+            cout << "Even"<< endl;
+        }
+    }
+    else{
+        cout << "Even" << endl;
+    }
+}
